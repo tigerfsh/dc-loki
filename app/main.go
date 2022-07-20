9e5bf10e247b09608580baf905c336b76066826a
tigerfsh <mrsmish@yahoo.com>
2022-07-20 19:22:24 +0800

    add app

---
+++ app/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"encoding/json"
+
+	"github.com/gin-gonic/gin"
+	"go.uber.org/zap"
+)
+
+func main() {
+	rawJSON := []byte(`{
+		"level": "debug",
+		"encoding": "json",
+		"outputPaths": ["stdout", "/tmp/app.log"],
+		"errorOutputPaths": ["stderr"],
+		"initialFields": {"foo": "bar"},
+		"encoderConfig": {
+		  "messageKey": "message",
+		  "levelKey": "level",
+		  "levelEncoder": "lowercase"
+		}
+	  }`)
+
+	var cfg zap.Config
+	if err := json.Unmarshal(rawJSON, &cfg); err != nil {
+		panic(err)
+	}
+	logger, err := cfg.Build()
+	if err != nil {
+		panic(err)
+	}
+	defer logger.Sync()
+	sugar := logger.Sugar()
+
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+		sugar.Infow("health check", "path", "/ping")
+	})
+	r.Run("0.0.0.0:8080") // listen and serve on 0.0.0.0:8080
+}
